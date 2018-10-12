a07c9be9e0090662b499bd7f88c51646a92e786e
Sam Carlberg <sam.carlberg@gmail.com>
2018-10-12 16:45:02 -0400

    Make sample-app a trivial application with jlink support
    Requires manually setting jlink task dependencies, but that will be fixed later

--- samples/simple-jar/build.gradle.kts
+++ samples/simple-jar/build.gradle.kts
@@ -1,5 +1,29 @@
+import org.apache.tools.ant.taskdefs.optional.jlink.JlinkTask
+import org.gradleweaver.plugins.jlink.JLinkTask
+
+plugins {
+    `application`
+}
+
 jlink {
-    "example" {
+    "release image" {
         useMinimalImage()
+        applicationJar = tasks.getByName<Jar>("jar").archivePath
+    }
+}
+
+tasks.withType<JLinkTask>().configureEach {
+    if (name == "jlinkGenerateReleaseImage") {
+        dependsOn("jar")
+    }
+}
+
+application {
+    mainClassName = "org.gradleweaver.plugin.sample.jlink.SampleApp"
+}
+
+tasks.withType<Jar>().configureEach {
+    manifest {
+        attributes("Main-Class" to application.mainClassName)
     }
 }
---
+++ samples/simple-jar/src/main/java/org/gradleweaver/plugin/sample/jlink/SampleApp.java
@@ -0,0 +1,7 @@
+package org.gradleweaver.plugin.sample.jlink;
+
+public class SampleApp {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}

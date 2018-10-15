861323a902895c0e98f5b871b396b15f3ce07538
Sam Carlberg <sam.carlberg@gmail.com>
2018-10-15 17:49:53 -0400

    Fix tests, make root check task rely on sample project jlink execution

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ project(":samples") {
 
 tasks.named<Task>("check") {
     dependsOn(gradle.includedBuild("simple-jlink").task(":check"))
+    dependsOn(findProject(":samples:javafx-app")!!.tasks.getByName("jlinkGenerateReleaseImage"))
+    dependsOn(findProject(":samples:simple-jar")!!.tasks.getByName("jlinkGenerateReleaseImage"))
 }
 
 apply(from = "gradle/wrapper.gradle.kts")

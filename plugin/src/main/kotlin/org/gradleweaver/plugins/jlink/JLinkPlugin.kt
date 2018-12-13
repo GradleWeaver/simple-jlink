d3c074026228b97d38c7592ff84ab22e58e173a7
Sam Carlberg <sam.carlberg@gmail.com>
2018-12-13 12:50:52 -0500

    Remove unused import

--- plugin/src/main/kotlin/org/gradleweaver/plugins/jlink/JLinkPlugin.kt
+++ plugin/src/main/kotlin/org/gradleweaver/plugins/jlink/JLinkPlugin.kt
@@ -3,7 +3,6 @@ package org.gradleweaver.plugins.jlink
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ApplicationPlugin
-import org.gradle.api.tasks.bundling.Compression
 import org.gradle.api.tasks.bundling.Tar
 import org.gradle.api.tasks.bundling.Zip
 import org.gradle.kotlin.dsl.create
